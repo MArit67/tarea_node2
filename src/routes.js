d22306733b799d5ca4106b201f2a9ed52f46b3ae
Mariana-Acevedo <mariana.acevedocg@gmail.com>
2025-11-12 20:11:28 -0500

    1 inicialización del proyecto con Express + estructura base

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+// src/routes.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => res.json({ ok: true, message: 'API running' }));
+
+module.exports = router;
